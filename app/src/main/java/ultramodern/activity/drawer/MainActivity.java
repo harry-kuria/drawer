0a4feb885bcdc0e576ea7b711c445a73aa0ae332
harry-kuria <harrisonkuria254@gmail.com>
2022-08-11 23:58:23 +0300

    drawer

---
+++ app/src/main/java/ultramodern/activity/drawer/MainActivity.java
@@ -0,0 +1,33 @@
+package ultramodern.activity.drawer;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+    private DrawerLayout mDrawer;
+    private ActionBarDrawerToggle mToggle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mDrawer = findViewById(R.id.main);
+        mToggle = new ActionBarDrawerToggle(this,mDrawer,R.string.Open,R.string.Close);
+        mDrawer.addDrawerListener(mToggle);
+        mToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (mToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
